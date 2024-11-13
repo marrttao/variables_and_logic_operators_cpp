6eccf2104c737378bd0d95388ed46908f0e9c377
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-13 19:36:12 +0200

    completed task 2

--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main() {
-	return 0;
+    int grn, copiyki;
+    cout << "Enter the number of grn: ";
+    cin >> grn;
+    cout << "Enter the number of copiyki: ";
+    cin >> copiyki;
+
+    cout << "Corrected amount: " << (grn + copiyki / 100) << " grn " << (copiyki % 100) << " copiyki" << endl;
+    return 0;
 }
