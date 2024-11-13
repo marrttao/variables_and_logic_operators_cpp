a91a2a5dc42c16b7436008723d824c69a7fd8d89
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-13 19:49:17 +0200

    completed task 4

--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
-	return 0;
+    float scale, Distance_On_Map;
+    cout << "Enter the input data:" << endl;
+    cout << "Map scale (kilometers per centimeter) -> ";
+    cin >> scale;
+    cout << "Distance between points representing settlements (cm) -> ";
+    cin >> Distance_On_Map;
+    float Settlements_Distance = scale * Distance_On_Map;
+    cout << "Distance between settlements: " << Settlements_Distance << " km" << endl;
+    return 0;
 }
