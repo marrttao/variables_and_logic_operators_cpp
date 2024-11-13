79f4ce6b7ed2cd06418c1c3b5da29c50395a62af
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-13 20:04:59 +0200

    completed task 5

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+#define _USE_MATH_DEFINES 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-	return 0;
+    double r;
+    cout << "Enter the radius of the ball: ";
+    cin >> r;
+    cout << endl << "The volume of the ball is: " << (4.0 / 3.0) * M_PI * (r * r * r) << " cm in cube" << endl;
+    return 0;
 }
