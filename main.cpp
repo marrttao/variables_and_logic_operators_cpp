ded1f0a76d74b39751111274a899232afd5a05f1
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-13 19:45:18 +0200

    completed task 3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <iomanip> 
 using namespace std;
 
 int main() {
-	return 0;
+    float length, width, height;
+    cout << "Length (cm) -> ";
+    cin >> length;
+    cout << "Width (cm) -> ";
+    cin >> width;
+    cout << "Height (cm) -> ";
+    cin >> height;
+
+    float volume = length * width * height;
+    cout << "Volume: " << fixed << setprecision(2) << volume << " cubic cm" << endl;
+
+    return 0;
 }
