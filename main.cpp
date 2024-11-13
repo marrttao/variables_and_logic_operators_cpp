92bd58d89daa3a2aa41e90b0a92091225a8e7d1d
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-13 19:08:22 +0200

    completed task 1

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main() {
+	int year;
+	cout << "Enter a year: ";
+	cin >> year;
+	int days = 365 + (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+	cout << "The number of days in the year " << year << " is " << days << endl;
 	return 0;
 }
